5d3b5642a9ab31e7b507ebc6365f9e75827ba1e1
Soudeh Babaeizadeh <babaeizadeh.s@gmail.com>
2025-01-28 17:29:07 +0330

    modification

--- src/registry/index.ts
+++ src/registry/index.ts
@@ -1,7 +1,5 @@
 import { examples } from "@/registry/registry-exmples";
 import type { Registry } from "@/registry/schema";
 import { extensions } from "./registry-extensions";
-// import { toolbarExamples } from "./registry-toolbar-examples";
-// import { toolbars } from "./registry-toolbars";
 
 export const registry: Registry = [...examples, ...extensions];
